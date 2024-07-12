281f182fd45d32604588eff32782f2b776f4e0bf
mrzhu666 <1052712811@qq.com>
2024-07-12 14:04:40 +0800

    init

---
+++ kafka-consumer/src/main/java/org/mrzhuyk/practice/KafkaConfig.java
@@ -0,0 +1,26 @@
+package org.mrzhuyk.practice;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+    
+    public static final String TOPIC_1 = "topic1";
+    
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name(TOPIC_1)
+            .partitions(1)
+            .replicas(1)
+            .build();
+    }
+    
+    @KafkaListener(id = "myId", topics = TOPIC_1)
+    public void listen(String in) {
+        System.out.println(in);
+    }
+}
---
+++ kafka-producer/src/main/java/org/mrzhuyk/practice/ProducerController.java
@@ -0,0 +1,7 @@
+package org.mrzhuyk.practice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProducerController {
+}
